8879d7d4e9c96b99613edfb4a19f00113eca7b99
ecuadros <ecuadros@spc.org.pe>
2026-01-29 12:02:17 -0500

    corregir

--- foreach.h
+++ foreach.h
@@ -18,13 +18,13 @@ Iterator FirstThat(Iterator begin, Iterator end, FuncObj fn, Args ...args){
     auto iter = begin;
     for(; iter != end ; ++iter)
         if( fn(*iter, args...) )
-            return *iter;
+            return iter;
     return end;
 }
 
 template <typename Container, typename FuncObj, typename ...Args>
 auto FirstThat(Container &container, FuncObj fn, Args ...args){
-    return FirstThat(container.begin(), container.end(), fn, args...)
+    return FirstThat(container.begin(), container.end(), fn, args...);
 }
 
 #endif
